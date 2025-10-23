b81316ee4c97e1cf3bc021837cc83634e27b3368
VencedorLOL <127039841+VencedorLOL@users.noreply.github.com>
2025-10-23 21:46:43 +0200

    Report now works as intended.

--- core/src/main/java/com/mygdx/game/items/Report.java
+++ core/src/main/java/com/mygdx/game/items/Report.java
@@ -6,20 +6,22 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import static com.mygdx.game.Settings.print;
+
 public class Report {
 	File report;
 	Report(){
 		try {
-			new File(Gdx.files.getLocalStoragePath() + "/Table").mkdir();
-			report = new File("times/PATimesTable.txt");
-			report.createNewFile();
+			print("Was folder created? " + new File(Gdx.files.getLocalStoragePath() + "/Table").mkdir());
+			report = new File(Gdx.files.getLocalStoragePath() + "Table/times.txt");
+			print("Was file created? "+report.createNewFile());
 
-		} catch (IOException ignored) {}
+		} catch (IOException ignored) {print("nope");}
 	}
 
 
 	public void writeFile(long time){
-		try (FileWriter reportReader = new FileWriter("times/times.txt",true)){reportReader.write("Time: "+time +"ns" + "\n");
+		try (FileWriter reportReader = new FileWriter(Gdx.files.getLocalStoragePath() + "Table/times.txt",true)){reportReader.write("Time: "+time +"ns" + "\n");
 		} catch (IOException ignored){}
 	}
 
