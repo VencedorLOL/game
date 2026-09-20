8a6f2c604451da26b0a3d64eb0b6f69a6f187f9f
VencedorLOL <127039841+VencedorLOL@users.noreply.github.com>
2026-09-20 22:03:02 +0200

    Version C.2.2 Implemented (but not tested) a way to save an arbitrary number of flags to the savefile.

--- core/src/main/java/com/mygdx/game/Savefile.java
+++ core/src/main/java/com/mygdx/game/Savefile.java
@@ -9,16 +9,21 @@ import com.mygdx.game.items.guielements.ClassesCards;
 import java.io.*;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 import static com.mygdx.game.GlobalVariables.classSlots;
 import static com.mygdx.game.Settings.print;
 import static com.mygdx.game.Settings.printErr;
+import static com.mygdx.game.Utils.indexForwardClosestToTarget;
 import static com.mygdx.game.items.characters.ClassStoredInformation.ClassInstance.classes;
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.valueOf;
 
 @SuppressWarnings("all")
 public class Savefile {
-
+	ArrayList<Flag> flags = new ArrayList<>();
 	File savefile;
 	Savefile(){
 		try {
@@ -32,16 +37,82 @@ public class Savefile {
 		} catch (FileNotFoundException ignored){}
 	}
 
+	/**
+	 * This returns the requested flag. If it doesn't exist, it makes one with the specified defaultValue.
+	 */
+	public String getOrMakeFlag(int pos, String value, String defaultValue){
+		String valu = getFlag(pos,value);
+		if(valu == null)
+			setFlag(pos,defaultValue);
+		return valu;
+	}
+
+	/**
+	 * This returns the requested flag. If it doesn't exist, it makes one with the specified defaultValue.
+	 */
+	public String getOrMakeFlag(String name, String value, String defaultValue){
+		String valu = getFlag(name,value);
+		if(valu == null)
+			setFlag(name,defaultValue);
+		return valu;
+	}
+
+
+	public String getFlag(int pos, String value){
+		for(Flag f : flags){
+			if(f.pos() == pos && pos != MAX_VALUE)
+				return f.value;
+		} printErr("No flag set with the requested position: " + pos); return null;
+	}
+
+	public String getFlag(String name, String value){
+		for(Flag f : flags){
+			if(f.name == name && name != null)
+				return f.value;
+		} printErr("No flag set with the requested name: " + name); return null;
+	}
+
+
+	public void setFlag(int pos, String value){
+		for(Flag f : flags)
+			if(f.pos() == pos && pos != MAX_VALUE) {
+				f.value = value;
+				return;
+			}
+		flags.add(new Flag(pos,value));
+	}
+
+	public void setFlag(String name, String value){
+		for(Flag f : flags)
+			if(f.name == name && name != null) {
+				f.value = value;
+				return;
+			}
+		flags.add(new Flag(name,value));
+	}
+
+
 	public void readFile() throws FileNotFoundException {
 		Scanner fileReader = new Scanner(new FileReader(savefile));
 		ArrayList<String> code = new ArrayList<>();
 		String line;
-		while (fileReader.hasNext()){
+		while (fileReader.hasNext())
 			code.add(fileReader.next());
-		}
+		int flagStart = -1;
 		if(!code.isEmpty()) {
+			for(int i = 0 ; i < code.size(); i++){
+				if(flagStart != -1){
+					if(code.get(i).indexOf(0) == '\"')
+						flags.add(new Flag(indexForwardClosestToTarget('\"',code.get(i),1),code.get(i).substring(code.get(i).indexOf(":")+2)));
+					else
+						flags.add(new Flag(i-flagStart,code.get(i).substring(code.get(i).indexOf(":")+2)));
+				}
+				if((code.get(i).equals("\n") || code.get(i).equals("")) && flagStart != -1)
+					flagStart = i;
+			}
+
 			line = code.get(0);
-			if(line.contains(",")) {
+			if(line.contains(",") && !line.contains(":")) {
 				String[] types = line.split(",");
 				for (int i = 0; i < types.length; i++) {
 					ClassAndEquipmentChanger.ClassObject temp = null;
@@ -55,7 +126,8 @@ public class Savefile {
 				}
 			}
 		}
-		for (int i = 1; i < code.size(); i++){
+		flagStart = flagStart == -1 ? code.size() : flagStart;
+		for (int i = 1; i < flagStart; i++){
 			line = code.get(i);
 			if(line.contains(",") && line.indexOf(",") == line.lastIndexOf(",")){
 				if (!line.substring(0, line.indexOf(",")).equals("-1")) {
@@ -74,6 +146,10 @@ public class Savefile {
 
 	}
 
+	/**
+	 * DO NOT USE. I DID NOT ADAPT IT TO THE NEW SYSTEM.
+	 */
+	@Deprecated
 	public void writeLine(int line) {
 		try {
 			ArrayList<String> code = new ArrayList<>(Files.readAllLines(savefile.toPath()));
@@ -108,6 +184,7 @@ public class Savefile {
 
 	public void writeFile() {
 		try {
+			// Saved classes and equipment
 			ArrayList<String> code = new ArrayList<>();
 			for (int i = 0; i < classes.length; i++) {
 				if (i == 0) {
@@ -122,8 +199,129 @@ public class Savefile {
 				} else
 					code.add(getWeapInt(i) + "," + getShieldInt(i));
 			}
+			code.add("");
+			// Other flags
+			ArrayList<Flag> numbered = new ArrayList<>();
+			ArrayList<Flag> named = new ArrayList<>();
+			int biggestPos = -1;
+			for(Flag f : flags)
+				if(f.pos() != MAX_VALUE) {
+					numbered.add(f);
+					biggestPos = f.pos() > biggestPos ? f.pos() : biggestPos;
+				} else
+					named.add(f);
+
+			for (int i = 0; i < Math.max(biggestPos,flags.size()); i++){
+				Flag flag = null;
+				for(Flag f : numbered)
+					if(f.pos() == i) {
+						flag = f;
+						code.add(": " + f.value);
+						break;
+					}
+				if(flag != null)
+					numbered.remove(flag);
+				else {
+					for(Flag f : named){
+						flag = f;
+						code.add("\"" + f.name + "\": " + f.value);
+						break;
+					} if(flag != null)
+						named.remove(flag);
+				}
+			}
+
 			Files.write(savefile.toPath(), code);
 		} catch (IOException ignored){printErr("Failed to make savefile at writeFile in Savefile class");}
+	}
+
+
+
+	public float getFFloat(String name, String value){
+		try {
+			return Float.valueOf(getFlag(name, value));
+		} catch (NumberFormatException ignored) {printErr("Could not convert to float the flag named: " + name + " because the flag is: " + value);}
+		return 0;
+	}
+
+	public float getFFloat(int pos, String value){
+		try {
+			return Float.valueOf(getFlag(pos, value));
+		} catch (NumberFormatException ignored) {printErr("Could not convert to float the flag numbered: " + pos + " because the flag is: " + value);}
+		return 0;
+	}
+
+	public int getFInt(String name, String value){
+		try {
+			return Integer.valueOf(getFlag(name, value));
+		} catch (NumberFormatException ignored) {printErr("Could not convert to int the flag named: " + name + " because the flag is: " + value);}
+		return 0;
+	}
+
+	public int getFInt(int pos, String value){
+		try {
+			return Integer.valueOf(getFlag(pos, value));
+		} catch (NumberFormatException ignored) {printErr("Could not convert to int the flag numbered: " + pos + " because the flag is: " + value);}
+		return 0;
+	}
+
+	public boolean getFBool(String name, String value){
+		try {
+			return Boolean.valueOf(getFlag(name, value));
+		} catch (NumberFormatException ignored) {printErr("Could not convert to boolean the flag named: " + name + " because the flag is: " + value);}
+		return false;
+	}
+
+	public boolean getFBool(int pos, String value){
+		try {
+			return Boolean.valueOf(getFlag(pos, value));
+		} catch (NumberFormatException ignored) {printErr("Could not convert to boolean the flag numbered: " + pos + " because the flag is: " + value);}
+		return false;
+	}
+
+	public void setFFloat(int pos, float value){
+		setFlag(pos,value+"");
+	}
+
+	public void setFFloat(String name, float value){
+		setFlag(name,value+"");
+	}
+
+	public void setFInt(int pos, int value){
+		setFlag(pos,value+"");
+	}
+
+	public void setFInt(String name, int value){
+		setFlag(name,value+"");
+	}
+
+	public void setFBool(int pos, boolean value){
+		setFlag(pos,value+"");
+	}
+
+	public void setFBool(String name, boolean value){
+		setFlag(name,value+"");
+	}
+
+	public static class Flag{
+		int pos = MAX_VALUE;
+		String name;
+		String value;
+
+		Flag(String name, String value){
+			this.name = name;
+			this.value = value;
+		}
+
+		Flag(int pos, String value){
+			this.pos = pos;
+			this.value = value;
+		}
+
+		public int pos(){
+			return pos;
+		}
+
 	}
 
 
